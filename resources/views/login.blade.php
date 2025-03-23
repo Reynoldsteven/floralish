916d2d68e66e8ffc7d49cdfb2d8e5f9f3b41678e
Reynoldsteven <reynoldsteven2506@gmail.com>
2025-03-23 22:23:14 +0700

    Pesan commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,6 @@
+<h1>Login Page</h1>
+<form>
+    <input type="text" placeholder="Email">
+    <input type="password" placeholder="Password">
+    <button type="submit">Login</button>
+</form>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,42 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ListItemController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/login', [LoginController::class, 'index']);
+Route::get('/dashboard', [DashboardController::class, 'index']);
+Route::get('/items', [ListItemController::class, 'index']); 
+
+
+// use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\ListBarangController;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/welcome', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/user/{id}', function ($id) {
+//     return 'User dengan ID ' . $id;
+// });
+
+// Route::prefix('admin')->group(function () {
+//     Route::get('/dashboard', function () { // "route::get" sebaiknya "Route::get"
+//         return 'Admin Dashboard';
+//     });
+
+//     Route::get('/users', function () {
+//         return 'Admin Users';
+//     });
+// });
+
+// // Route::get('/listbarang/{id}/{nama}', function($id, $nama){
+// //     return view('list_barang', compact('id', 'nama'));
+// // });
+
+// route::get('/listbarang/{id}/{nama}', [ListBarangController::class, 'tampilkan']);
